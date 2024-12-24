6105cf17a297a06f027a90182a48de13fdd2b309
leehuanyu <4b2g0001@stust.edu.tw>
2024-12-24 15:24:26 +0800

    新增學生資料初始化及 UI 元件
    在 `MainWindow.xaml` 中，新增了 `Grid.RowDefinitions` 和 `Grid.ColumnDefinitions`，並在 `Grid` 中新增了一個 `StackPanel` 和一個 `StatusBar`。`StackPanel` 包含一個 `Label` 和一個 `ComboBox`，而 `StatusBar` 包含一個 `StatusBarItem`，其中有一個 `Label`。
    
    在 `MainWindow.xaml.cs` 中，移除了多個 `using` 指令，並新增了一個 `List<Student>` 變數。新增了一個 `InitailizeData` 方法，用於初始化學生資料並將其設置為 `ComboBox` 的 `ItemsSource`，同時將 `ComboBox` 的 `SelectedIndex` 設置為 0。
    
    新增了一個 `Student.cs` 檔案，定義了一個 `Student` 類別，包含 `StudentId` 和 `StudentName` 屬性，並覆寫了 `ToString` 方法以返回學生的 ID 和姓名。

--- 2024_WpfApp6/MainWindow.xaml.cs
+++ 2024_WpfApp6/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace _2024_WpfApp6
 {
@@ -16,9 +7,20 @@ namespace _2024_WpfApp6
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students = new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
+            InitailizeData();
+        }
+
+        private void InitailizeData()
+        {
+            students.Add(new Student { StudentId = "S001", StudentName = "陳小明" });
+            students.Add(new Student { StudentId = "S002", StudentName = "林小華" });
+            students.Add(new Student { StudentId = "S003", StudentName = "張小英" });
+            cmbStudent.ItemsSource = students;
+            cmbStudent.SelectedIndex = 0;
         }
     }
 }
---
+++ 2024_WpfApp6/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace _2024_WpfApp6
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+        public override string ToString()
+        {
+            return $"{StudentId} {StudentName}";
+        }
+    }
+}
